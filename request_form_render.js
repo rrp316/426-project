2ac69dbbd7f56391de1559bdba8a369a641d0087
Raj Patel <rrp316@outlook.com>
2019-11-23 20:41:09 -0500

    Added TODO comments on request_form_render

--- request_form_render.js
+++ request_form_render.js
@@ -1,6 +1,6 @@
 
 const loadRequestForm = function() {
-  console.log(`entered function`);
+  //TODO: standardize html to match the format of the site
     let body = $(`body`);
     body.append(`<h1 class = "title">Request Maintenance Form</h1>`);
     let container = $(`<div class = "container"></div>`);
@@ -17,13 +17,15 @@ const loadRequestForm = function() {
     <input name = "state" class="input" type="text">`);
     form.append(`<label class = "label">ZIP Code</label>
     <input name = "zip" class="input" type="text">`);
-  //add other form items
+  //add other form items if needed
   form.append(`<label class = "label">Description Of Issue<label>
   <textarea name = "descript" class="textarea"></textarea>`);
   form.append(`<button class = "submit" type = "submit">Submit</button>`);
   container.append(form);
   body.append(container);
 }
+
+//TODO: add handle for submit and make sure all parts are filled
 
 
 $(function() {
