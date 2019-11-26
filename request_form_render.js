9b437a9db27bb3d2d9d9a65da784a3bf80c54eef
Joshua Pack <joshuapack1@gmail.com>
2019-11-25 21:39:33 -0500

    added bulma to maintainence form submit button

--- request_form_render.js
+++ request_form_render.js
@@ -1,6 +1,6 @@
 
-const loadRequestForm = function() {
-  //TODO: standardize html to match the format of the site
+const loadRequestForm = function () {
+    //TODO: standardize html to match the format of the site
     let body = $(`body`);
     body.append(`<h1 class = "title">Request Maintenance Form</h1>`);
     let container = $(`<div class = "container"></div>`);
@@ -17,17 +17,17 @@ const loadRequestForm = function() {
     <input name = "state" class="input" type="text">`);
     form.append(`<label class = "label">ZIP Code</label>
     <input name = "zip" class="input" type="text">`);
-  //add other form items if needed
-  form.append(`<label class = "label">Description Of Issue<label>
+    //add other form items if needed
+    form.append(`<label class = "label">Description Of Issue<label>
   <textarea name = "descript" class="textarea"></textarea>`);
-  form.append(`<button class = "submit" type = "submit">Submit</button>`);
-  container.append(form);
-  body.append(container);
+    form.append(`<button class = "button is-link submit" type = "submit">Submit</button>`);
+    container.append(form);
+    body.append(container);
 }
 
 //TODO: add handle for submit and make sure all parts are filled
 
 
-$(function() {
+$(function () {
     loadRequestForm();
 });
