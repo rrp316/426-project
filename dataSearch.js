7e5f89f432389d6e7d0cb6e978205f60c845378b
Joshua Pack <joshuapack1@gmail.com>
2019-11-27 22:25:25 -0500

    working weather api for temp and wind speed

--- dataSearch.js
+++ dataSearch.js
@@ -14,7 +14,15 @@ export const handleAddressSearch = async () => {
 
     //call weather api 
     let weatherResult = await callWeatherAPI(latitude, longitude);
-    console.error.log(weatherResult);
+    let temp = weatherResult['temp'];
+    temp = (temp * 9 / 5) + 32; //converting from Celsius to Fahrenheit
+    let description = weatherResult['weather']['description'];
+    let wind_spd = weatherResult['wind_spd'];
+    wind_spd = wind_spd * 2.237; //converting from m/s to mph
+    wind_spd.toFixed(2); //rounding to 2 decimal places
+    console.log(`temp: ${temp}`);
+    console.log(`description: ${description}`);
+    console.log(`wind_spd: ${wind_spd}`);
 };
 
 $(document).ready(function () {
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,10 +1,10 @@
 async function callWeatherAPI(latitude, longitude) {
-    let u = `https://api.darksky.net/forecast/71cea83be9deb275e13b11f1723a505a/${latitude},${longitude}`;
+    let u = `https://api.weatherbit.io/v2.0/current?key=2d45382b691e43a1b23c460134417d3c&lat=${latitude}&lon=${longitude}`;
     let result = await axios({
         method: 'get',
         url: u,
-        mode: 'no-cors',
     });
 
+    result = result['data']['data'][0];
     return result;
 }
