c5fd374b212d95b7d4cd6d3e8168a4b9d43b0222
Joshua Pack <joshuapack1@gmail.com>
2019-11-27 21:20:08 -0500

    added geocoding API

--- dataSearch.js
+++ dataSearch.js
@@ -1,7 +1,12 @@
 export const handleAddressSearch = async () => {
     let input = $(`#addressSearchInput`).val();
-    console.log(`input: ${input}`);
-}
+    input = parseInput(input);
+    let result = await callGeoCodeAPI(input);
+    let latitude = result['Latitude'];
+    let longitude = result['Longitude'];
+    console.log(`Latitude: ${latitude}`);
+    console.log(`Longitude: ${longitude}`);
+};
 
 $(document).ready(function () {
     $('#addressSearchBtn').on('click', handleAddressSearch);
--- geocode.js
+++ geocode.js
@@ -5,13 +5,18 @@ function parseInput(s) {
     let currentWord = [];
     for (let i = 0; i < s.length; i++) {
         let c = s.charAt(i);
-        if (c != ' ') {
+        if (c != ' ' && c != ',') {
             currentWord.push(c);
-        } else {
+        } else if (currentWord.length != 0) {
             let string = currentWord.join("");
             words.push(string);
             currentWord = [];
         }
+    }
+
+    if (currentWord.length != 0) {
+        let string = currentWord.join("");
+        words.push(string);
     }
 
     let result = '';
@@ -21,5 +26,19 @@ function parseInput(s) {
     });
     result = result.substring(0, result.length - 1);
 
+    return result;
+};
+
+//expects input like: Chapel+Hill+North+Carolina
+async function callGeoCodeAPI(s) {
+    const app_id = '3boscW8lytN3EJTQTIBS';
+    const app_code = 'r9Lizz8pwlpdqNNBgVnAfA';
+    const searchtext = s;
+    let result = await axios({
+        method: 'get',
+        url: `https://geocoder.api.here.com/6.2/geocode.json?app_id=${app_id}&app_code=${app_code}&searchtext=${searchtext}`,
+    });
+
+    result = result['data']['Response']['View'][0]['Result'][0]['Location']['DisplayPosition'];
     return result;
 }
