a2c3eb6bb51255fbf478276f99487f7d6cf4617d
Joshua Pack <joshuapack1@gmail.com>
2019-12-03 18:30:55 -0500

    added onclick listener for request maintenance form

--- requestM.js
+++ requestM.js
@@ -2,8 +2,22 @@ export const handleFormSubmit = async () => {
     var form = $(`#requestMForm`);
 
     let firstName = form.find('input[name=first]').val();
+    let lastName = form.find('input[name=last]').val();
+    let address = form.find('input[name=address]').val();
+    let city = form.find('input[name=city]').val();
+    let state = form.find('input[name=state]').val();
+    let zip = form.find('input[name=zip]').val();
+    let description = form.find('textarea[name=description]').val();
 
-    alert(`firstName: ${firstName}`);
+    console.log(`${firstName}`);
+    console.log(`${lastName}`);
+    console.log(`${address}`);
+    console.log(`${city}`);
+    console.log(`${state}`);
+    console.log(`${zip}`);
+    console.log(`${description}`);
+
+    //then axios stuff, removing the above console logs
 };
 
 $(document).ready(function () {
