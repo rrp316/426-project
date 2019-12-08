7a847228db2c603489423124bac634dc0957b9ff
Joshua Pack <joshuapack1@gmail.com>
2019-12-08 14:24:29 -0500

    working on moving active requests to my jobs

--- site/activeRequests.js
+++ site/activeRequests.js
@@ -7,16 +7,29 @@ async function getActiveRequests() {
     return activeRequests;
 }
 
-export const handleAddRequestToMyJobs = async function () {
-    /** TO-DO:
-    * Raj if you can just fill out the below request with whatever
-    * data you need, I can then figure out how to get that data from
-    * the DOM. 
-    */
-    let resourceName;
+export const handleAddRequestToMyJobs = async function (event) {
+    let resourceName = event.currentTarget.id;
     let userName = localStorage.getItem('un');
+    console.log(`resourceName: ${resourceName}`);
+    console.log(`userName: ${userName}`);
     let jwt = localStorage.getItem('jwt');
-    const result = await axios({
+    let firstName = $(`#${resourceName}_firstName`).text();
+    let lastName = $(`#${resourceName}_lastName`).text();
+    let address = $(`#${resourceName}_address`).text();
+    let city = $(`#${resourceName}_city`).text();
+    let state = $(`#${resourceName}_state`).text();
+    let zip = $(`#${resourceName}_zip`).text();
+    let description = $(`#${resourceName}_description`).text();
+    /*console.log(firstName);
+    console.log(lastName);
+    console.log(address);
+    console.log(city);
+    console.log(state);
+    console.log(zip);
+    console.log(description);*/
+
+
+    await axios({
         method: 'POST',
         url: `http://localhost:3000/account/${userName}/${resourceName}`,
         data: {
@@ -35,6 +48,7 @@ export const handleAddRequestToMyJobs = async function () {
     });
 
     await axios.delete(`http://localhost:3000/public/requests/${resourceName}`);
+
     //let me know if this delete works I haven't tested it
 
     //location.reload();
@@ -49,12 +63,18 @@ document.body.onload = async function () {
             <div class="card">
                 <div class="card-content">
                     <div class="content">
-                        ${activeRequests[i]['address']} ${activeRequests[i]['city']}
-                        <br />
-                        ${activeRequests[i]['state']} ${activeRequests[i]['zip']}
+                        <p><span id="${i}_firstName">${activeRequests[i]['firstName']}</span> <span id="${i}_lastName">${activeRequests[i]['lastName']}</span></p>
+                        <p><span id="${i}_address">${activeRequests[i]['address']}</span> <span id="${i}_city">${activeRequests[i]['city']}</span></p>
+                        <p><span id="${i}_state">${activeRequests[i]['state']}</span> <span id="${i}_zip">${activeRequests[i]['zip']}</span></p>
+                        <p>Description: <span id="${i}_description">${activeRequests[i]['description']}</span></p>
                     </div>
                 </div>
+                <button class="addToMyJobsBtn button is-link" id="${i}">Add to My Jobs</button>
+                <br />
+                <br />
             </div>
             <br />`);
     }
+
+    $('.addToMyJobsBtn').on('click', handleAddRequestToMyJobs);
 }
